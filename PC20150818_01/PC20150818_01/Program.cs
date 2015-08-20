3392c73e23598ff001ea67871414f971d1ea596c
Walter Gandarella <walter.wgbn@gmail.com>
2015-08-20 13:29:49 -0300

    inicio

---
+++ PC20150818_01/PC20150818_01/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PC20150818_01 {
+    class Program {
+        static void Main(string[] args) {
+
+            private String nome;
+
+            public String Nome {
+                get { return this.nome; }
+                set { this.nome = value; }
+            }
+        }
+    }
+}
