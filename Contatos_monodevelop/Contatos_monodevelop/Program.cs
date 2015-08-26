33e2c7ddd1cf56e99f8fa95ec6b75869e4e47989
Walter Gandarella <walter.wgbn@gmail.com>
2015-08-26 08:35:18 -0300

    20150826

---
+++ Contatos_monodevelop/Contatos_monodevelop/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Gtk;
+
+namespace Contatos_monodevelop {
+	class MainClass {
+		public static void Main (string[] args) {
+			Application.Init ();
+			MainWindow win = new MainWindow ();
+			win.Show ();
+			Application.Run ();
+		}
+	}
+}
