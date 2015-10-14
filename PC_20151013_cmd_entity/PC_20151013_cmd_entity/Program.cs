9cda7663409fe1c9b7464a3aabe7e54f19bc767e
Walter Gandarella <walter.wgbn@gmail.com>
2015-10-14 07:24:16 -0300

    2051013

---
+++ PC_20151013_cmd_entity/PC_20151013_cmd_entity/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PC_20151013_cmd_entity {
+    class Program {
+        static void Main(string[] args) {
+        }
+    }
+}
---
+++ PC_20151013_conexao_ADO/PC_20151013_conexao_ADO/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PC_20151013_conexao_ADO {
+    class Program {
+        static void Main(string[] args) {
+            SqlConnection conn = new SqlConnection("Data Source=localhost\\sqlexpress;Initial Catalog=csharp;Integrated Security=True");
+            conn.Open();
+            SqlCommand cmd = new SqlCommand("select Id, Nome from Categoria", conn);
+            
+            SqlDataReader r = cmd.EndExecuteReader();
+
+            while (r.Read()) {
+                Console.WriteLine("Categoria: {0}", r["Nome"].ToString());
+            }
+            conn.Close();
+        }
+    }
+}
